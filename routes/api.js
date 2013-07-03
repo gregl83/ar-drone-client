5973ec34fdcf0832db15c2908d8d5d5403a5f829
gregl83 <general@gregorylanglais.com>
2013-07-02 20:35:50 -0400

    Drone api setup
    Express basic setup and drone connection attempt.  Drone connection not working yet.

---
+++ routes/api.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+
+
+/*
+ * GET api takeoff.
+ */
+
+exports.takeoff = function(req, res){
+  client.takeoff();
+  res.json({result: true});
+};
+
+
+/*
+ * GET api land.
+ */
+
+exports.land = function(req, res){
+  client.land();
+  res.json({result: true});
+};
+
+
+/*
+ * GET api stop.
+ */
+
+exports.stop = function(req, res){
+  client.stop();
+  res.json({result: true});
+};
+
+
+/*
+ * GET api frontCam.
+ */
+
+exports.frontCam = function(req, res){
+  /*var stream = client.createPngStream({log: console.log});
+  stream.data(function(buffer) {
+    res.write(buffer);
+  });*/
+  res.send();
+};
