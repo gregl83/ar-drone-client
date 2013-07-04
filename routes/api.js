4abdbd41bef04d11d44c7f23f830b9f8614943dc
gregl83 <general@gregorylanglais.com>
2013-07-04 11:34:31 -0400

    Removed API calls in favor of socket.io commands
    This code was left over after merge with diversario pull request.

--- routes/api.js
+++ routes/api.js
@@ -3,35 +3,6 @@
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
-
-/*
- * GET api takeoff.
- */
-
-exports.takeoff = function() {
-  console.log('>> takeoff motherfuckers');
-  client.takeoff();
-};
-
-
-/*
- * GET api land.
- */
-
-exports.land = function() {
-  client.land();
-};
-
-
-/*
- * GET api stop.
- */
-
-exports.stop = function() {
-  client.stop();
-};
-
-
 /*
  * GET api frontCam.
  */
